3c76edbb9f13519b4d34c38cf2f6586ba022aafb
Max Lobur <max_lobur@outlook.com>
2017-05-03 16:06:01 +0300

    Replace own validation with custom req parser type

--- app/api.py
+++ app/api.py
@@ -15,29 +15,30 @@ limiter = Limiter(app, key_func=get_remote_address,
                   storage_uri="memory://")
 
 
+def coord(raw_coord):
+    try:
+        x, y = raw_coord.split(",")
+        return float(x), float(y)
+    except Exception:
+        raise ValueError("Coordinate must be in format latitude,longitude")
+
+
 class Directions(Resource):
     decorators = [limiter.limit("5 per minute")]
 
     @cache.cached(timeout=60*60*24, key_prefix=lambda *a, **kw: request.url)
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('orig', location='args', required=True)
-        parser.add_argument('dest', location='args', required=True)
+        parser.add_argument('orig', type=coord, location='args', required=True)
+        parser.add_argument('dest', type=coord, location='args', required=True)
         args = parser.parse_args(strict=True)
         try:
-            res = maps.directions(_parse_coord(args['orig']),
-                                  _parse_coord(args['dest']))
+            res = maps.directions(args['orig'], args['dest'])
         except maps.OutOfBounds as e:
             return {"error": str(e)}, 400
         return {"polyline": res}
 
-
 api.add_resource(Directions, '/')
-
-
-def _parse_coord(coord):
-    x, y = coord.split(",")
-    return float(x), float(y)
 
 
 if __name__ == '__main__':
